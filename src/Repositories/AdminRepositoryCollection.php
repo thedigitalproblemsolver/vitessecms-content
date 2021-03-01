47a11c86f9ccfbeccb6fb29a3e25085ae78941ab
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2021-03-01 18:43:19 +0100

    Moved beforesave

--- src/Repositories/AdminRepositoryCollection.php
+++ src/Repositories/AdminRepositoryCollection.php
@@ -2,8 +2,10 @@
 
 namespace VitesseCms\Content\Repositories;
 
+use VitesseCms\Datafield\Repositories\DatafieldRepository;
 use VitesseCms\Datagroup\Repositories\DatagroupRepository;
 use VitesseCms\Database\Interfaces\BaseRepositoriesInterface;
+use VitesseCms\Language\Repositories\LanguageRepository;
 
 class AdminRepositoryCollection implements BaseRepositoriesInterface
 {
@@ -17,12 +19,26 @@ class AdminRepositoryCollection implements BaseRepositoriesInterface
      */
     public $datagroup;
 
+    /**
+     * @var DatafieldRepository
+     */
+    public $datafield;
+
+    /**
+     * @var LanguageRepository
+     */
+    public $language;
+
     public function __construct(
         ItemRepository $itemRepository,
-        DatagroupRepository $datagroupRepository
+        DatagroupRepository $datagroupRepository,
+        DatafieldRepository $datafieldRepository,
+        LanguageRepository $languageRepository
     )
     {
         $this->item = $itemRepository;
         $this->datagroup = $datagroupRepository;
+        $this->datafield = $datafieldRepository;
+        $this->language = $languageRepository;
     }
 }
