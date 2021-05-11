441a127e8613f6a1d04ada69dda6a4e984ea7ae7
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2021-05-11 22:12:47 +0200

    Added pagination option to BlockMainContent.php and shop overview pages

--- src/Listeners/BlockMainContentListener.php
+++ src/Listeners/BlockMainContentListener.php
@@ -4,11 +4,14 @@ namespace VitesseCms\Content\Listeners;
 
 use Phalcon\Events\Event;
 use VitesseCms\Block\Forms\BlockForm;
+use VitesseCms\Form\Models\Attributes;
 
 class BlockMainContentListener
 {
     public function buildBlockForm(Event $event, BlockForm $form): void
     {
-        $form->addToggle('Use datagroup template', 'useDatagroupTemplate');
+        $form->addToggle('Use datagroup template', 'useDatagroupTemplate')
+            ->addNumber('Overview item limit', 'overviewItemLimit', (new Attributes())->setRequired())
+        ;
     }
 }
